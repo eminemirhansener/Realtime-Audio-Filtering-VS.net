71cb087378d09dca744f9687da9c3dfb34f20f79
eminemirhansener <eminvemirvehan@gmail.com>
2023-01-28 13:51:34 +0300

    Realtime Audio Filtering
    NAudio, ScottPlot, SoxSharp

---
+++ Audio Signal Processing 1/Audio Signal Processing 1/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Audio_Signal_Processing_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.scottPlotUC1 = new ScottPlot.ScottPlotUC();
+            this.scottPlotUC2 = new ScottPlot.ScottPlotUC();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // scottPlotUC1
+            // 
+            this.scottPlotUC1.ForeColor = System.Drawing.Color.Black;
+            this.scottPlotUC1.Location = new System.Drawing.Point(161, 26);
+            this.scottPlotUC1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.scottPlotUC1.Name = "scottPlotUC1";
+            this.scottPlotUC1.Size = new System.Drawing.Size(486, 195);
+            this.scottPlotUC1.TabIndex = 0;
+            // 
+            // scottPlotUC2
+            // 
+            this.scottPlotUC2.Location = new System.Drawing.Point(161, 241);
+            this.scottPlotUC2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.scottPlotUC2.Name = "scottPlotUC2";
+            this.scottPlotUC2.Size = new System.Drawing.Size(486, 195);
+            this.scottPlotUC2.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 441);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 441);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Input Device";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(383, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Time [ms]";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(361, 397);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Frequency [kHz]";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(203, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Amplitude";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(203, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Amplitude";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(203, 223);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Peak Frequency:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 476);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.scottPlotUC2);
+            this.Controls.Add(this.scottPlotUC1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ScottPlot.ScottPlotUC scottPlotUC1;
+        private ScottPlot.ScottPlotUC scottPlotUC2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Audio Signal Processing 1/Audio Signal Processing 1/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+using System.Numerics;
+using Accord.Math;
+using NAudio.Dsp;
+
+using complex = System.Numerics.Complex;
+using System.IO;
+using System.Security.Cryptography;
+using SoxSharp.Effects.Types;
+using SoxSharp.Effects;
+using SoxSharp;
+using System.Diagnostics;
+
+namespace Audio_Signal_Processing_1
+{
+    public partial class Form1 : Form
+    {
+
+        public WaveIn wi;
+        public BufferedWaveProvider bwp;
+        public Int32 envelopeMax;
+        public int deviceNumber;
+        public int frequency = 4000;
+        public int width = 3500;
+        public bool btn_clckd;
+
+        private int SampleRate = 44100; // Sound Card Sampling Rate
+        private int BufferSize = (int)Math.Pow(2, 11);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Datas are put into buffer.
+        void wi_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            bwp.AddSamples(e.Buffer, 0, e.BytesRecorded);
+        }
+
+        public void updateAudioGraph()
+        {
+
+            int frameSize = BufferSize;
+            var frames = new byte[frameSize];
+            bwp.Read(frames, 0, frameSize);
+            if (frames.Length == 0) return;
+            if (frames[frameSize - 2] == 0) return;
+
+            timer1.Enabled = false;
+
+            // convert it to int32 manually (and a double for scottplot)
+            int SAMPLE_RESOLUTION = 16;
+            int BYTES_PER_POINT = SAMPLE_RESOLUTION / 8;
+            Int32[] vals = new Int32[frames.Length / BYTES_PER_POINT];
+            double[] Ys = new double[frames.Length / BYTES_PER_POINT];
+            double[] Xs = new double[frames.Length / BYTES_PER_POINT];
+            double[] Ys2 = new double[frames.Length / BYTES_PER_POINT];
+            double[] Xs2 = new double[frames.Length / BYTES_PER_POINT];
+            for (int i = 0; i < vals.Length; i++)
+            {
+                // bit shift the byte buffer into the right variable format
+                byte hByte = frames[i * 2 + 1];
+                byte lByte = frames[i * 2 + 0];
+                vals[i] = (int)(short)((hByte << 8) | lByte);
+                Xs[i] = i;
+                Ys[i] = vals[i];
+                Xs2[i] = (double)i / Ys.Length * SampleRate / 1000.0; // units are in kHz
+            }
+
+            // update scottplot (PCM, time domain)
+            scottPlotUC1.Xs = Xs;
+            scottPlotUC1.Ys = Ys;
+
+            //update scottplot (FFT, frequency domain)
+            Ys2 = FFT(Ys);
+            scottPlotUC2.Xs = Xs2.Take(Xs2.Length / 2).ToArray();
+            scottPlotUC2.Ys = Ys2.Take(Ys2.Length / 2).ToArray();
+
+
+            // update the displays
+            scottPlotUC1.UpdateGraph();
+            scottPlotUC2.UpdateGraph();
+
+            Application.DoEvents();
+            scottPlotUC1.Update();
+            scottPlotUC2.Update();
+
+            calculatePeakFreq(Xs2, Ys2);
+
+            timer1.Enabled = true;
+
+        }
+
+        private void sox(int frequency, int width)
+        {
+
+            using (var sox = new Sox("C:\\Program Files (x86)\\sox-14-4-1\\sox.exe"))
+            {
+                sox.Effects.Add(new VolumeEffect(0, GainType.Db));
+                sox.Effects.Add(new BandPassFilterEffect(frequency, width));
+                sox.OnProgress += sox_OnProgress;
+                sox.Process("-d", "-d");
+            }
+
+        }
+        
+        // FFT converting function.
+        public double[] FFT(double[] data)
+        {
+            double[] fft = new double[data.Length];
+            complex[] fftComplex = new complex[data.Length];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                fftComplex[i] = new complex(data[i], 0.0);
+            }
+
+            Accord.Math.FourierTransform.FFT(fftComplex, Accord.Math.FourierTransform.Direction.Forward);
+
+            for(int i = 0; i < data.Length; i++)
+            {
+                fft[i] = fftComplex[i].Magnitude;
+                //fft[i] = 10 * Math.Log10(fft[i]); // to convert dB
+            }
+
+            return fft;
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //İnput Devices are shown in the combo box.
+            for (int i = 0; i < NAudio.Wave.WaveIn.DeviceCount; i++)
+            {
+                var inp_device = NAudio.Wave.WaveIn.GetCapabilities(i);
+                comboBox1.Items.Add(inp_device.ProductName);
+            }
+
+            deviceNumber  = comboBox1.SelectedIndex;
+        }
+
+        private void calculatePeakFreq(double[] XFreq, double[] YMag)
+        {
+            double max_valueMag = YMag.Max();
+            int indexOfMaxMag = YMag.IndexOf(max_valueMag);
+            double max_valueFreqKHz = XFreq[indexOfMaxMag];
+            double max_valueFreqHz = Math.Round(max_valueFreqKHz * 1000);
+
+            if(max_valueMag > 0.5)
+            {
+                label6.Text = "Peak Frequency: " + max_valueFreqHz + " [Hz]";
+            }
+
+            else
+                label6.Text = "Peak Frequency: " + 0 + " [Hz]";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            updateAudioGraph();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WaveIn wi = new WaveIn();
+            wi.DeviceNumber = deviceNumber;
+
+            if (comboBox1.SelectedIndex < 0)
+            {
+                MessageBox.Show("Select an Input Device", "Input Device Error");
+                return;
+            }
+
+            wi.WaveFormat = new NAudio.Wave.WaveFormat(SampleRate, 1);
+            wi.BufferMilliseconds = (int)((double)BufferSize / (double)SampleRate * 1000.0);
+
+            // create a wave buffer and start the recording
+            wi.DataAvailable += new EventHandler<WaveInEventArgs>(wi_DataAvailable);
+            bwp = new BufferedWaveProvider(wi.WaveFormat);
+            bwp.BufferLength = BufferSize * 2;
+
+            bwp.DiscardOnBufferOverflow = true;
+            wi.StartRecording();
+
+            updateAudioGraph();
+
+            Thread threadSox = new Thread(() => sox(frequency, width));
+            threadSox.Start();
+
+            timer1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            btn_clckd = true;
+        }
+
+        void sox_OnProgress(object sender, ProgressEventArgs e)
+        {
+            if (btn_clckd)
+                e.Abort = true;
+
+            btn_clckd = false;
+        }
+    }
+}
